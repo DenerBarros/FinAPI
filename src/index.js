ad5c397d9140e602600f2dba65d6e918fe933052
DenerBarros <denerbarros.eng@gmail.com>
2022-09-01 16:17:39 -0300

    Criação do Middleware para verificar se existe a conta com o CPF, e criação da rota /deposit com método post

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ const app = express();
 app.use(express.json());
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement - []
- */
+
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error: 'Customer not found'});
+    } 
+
+
+    request.customer = customer;
+    return next();
+}
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
@@ -36,13 +45,26 @@ app.post("/account", (request, response) => {
 
 });
 
-
-app.get("/statement/:cpf",(request, response) => {
-    const { cpf } = request.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
     return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 
