34782274af46e1b5764bffbed4a1ca17afd3cdd7
DenerBarros <denerbarros.eng@gmail.com>
2022-09-01 17:30:14 -0300

    Criado os requisitos de atualizar dados da conta do cliente e obter dados da conta do cliente

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -99,5 +100,35 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
     return response.status(201).send();
 
 });
+
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+
+
+app.get("/account", verifyIfExistsAccountCPF,  (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+
+});
+
 
 app.listen(3333);
