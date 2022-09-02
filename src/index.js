86b05ea9c2978c0fbce0b2468f1b196a0f78df22
DenerBarros <denerbarros.eng@gmail.com>
2022-09-02 08:42:26 -0300

    Feito das regras de retornar o balance e não deve excluir uma conta não existente

--- src/index.js
+++ src/index.js
@@ -130,5 +130,20 @@ app.get("/account", verifyIfExistsAccountCPF,  (request, response) => {
 
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+});
 
 app.listen(3333);
